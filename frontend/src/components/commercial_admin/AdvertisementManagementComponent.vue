32a23ae0c64bbbfca16729d3af22f755212db085
Krisztián Fehér <feher.krisztian1@gmail.com>
2020-11-28 14:55:26 +0100

    split commercial admin component to two seperate

---
+++ frontend/src/components/commercial_admin/AdvertisementManagementComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="mt-5">
+    <h3>Advertisement management</h3>
+  </div>
+</template>
+<script>
+//import swal from "sweetalert";
+
+// var tokenInHeader = {
+//   headers: {
+//     Authorization: "Bearer " + localStorage.getItem("jwt"),
+//   },
+// };
+
+export default {
+  data() {
+    return {
+    };
+  },
+  created: function () {
+  },
+  methods: {
+  },
+};
+</script>
