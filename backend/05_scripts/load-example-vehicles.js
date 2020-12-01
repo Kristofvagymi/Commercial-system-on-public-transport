73e92930e117930b3f507c81e49e7ed0dcca44db
Krisztián Fehér <feher.krisztian1@gmail.com>
2020-12-01 23:40:37 +0100

    modified script

--- backend/05_scripts/load-example-vehicles.js
+++ backend/05_scripts/load-example-vehicles.js
@@ -3,31 +3,31 @@ const mongoose = require("mongoose");
 const config = require("../04_config/db.config");
 
 mongoose
-  .connect(config.url, { useNewUrlParser: true })
-  .then(() => {
-    console.log("Database is connected");
-    loadvehicles();
-  })
-  .catch(err => {
-    console.log({ database_error: err });
-  });
+    .connect(config.url, { useNewUrlParser: true })
+    .then(() => {
+        console.log("Database is connected");
+        loadvehicles();
+    })
+    .catch(err => {
+        console.log({ database_error: err });
+    });
 
-function loadvehicles(){
-    const vehicle1 = new Vehicle({registrationNumber: 'ABC-123', countries: []});
-    const vehicle2 = new Vehicle({registrationNumber: 'SEX-420', countries: ["Pest"]});
-    const vehicle3 = new Vehicle({registrationNumber: 'LOL-000', countries: ["Pest", "Baranya"]});
+function loadvehicles() {
+    const vehicle1 = new Vehicle({ registrationNumber: 'ABC-123', countries: ["Vas", "Zala"] });
+    const vehicle2 = new Vehicle({ registrationNumber: 'DEF-420', countries: ["Pest", "Baranya"] });
+    const vehicle3 = new Vehicle({ registrationNumber: 'LOL-000', countries: ["Pest", "Baranya", "Somogy"] });
 
     console.log('default vehicles:\n');
 
-    vehicle1.save(function (err, saved) {
+    vehicle1.save(function(err, saved) {
         if (err) console.log(err);
         else console.log(saved)
     })
-    vehicle2.save(function (err, saved) {
+    vehicle2.save(function(err, saved) {
         if (err) console.log(err);
         else console.log(saved)
     })
-    vehicle3.save(function (err, saved) {
+    vehicle3.save(function(err, saved) {
         if (err) console.log(err);
         else console.log(saved)
     })
